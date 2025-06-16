1fb64415fb6ff48a52eb7cb648bcf19488d37b63
andrebrumdev <andre.brum@softlive>
2025-06-16 09:14:06 -0400

    feat: adiciona configuração de assetPrefix e habilita trailingSlash e reactStrictMode no Next.js

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     output: 'export',
+    assetPrefix: './',
     basePath: process.env.PAGES_BASE_PATH,
+    trailingSlash: true,
+    reactStrictMode: true,
+    // images: {
+    //     unoptimized: true,
+    // },
 };
 
 export default nextConfig;
