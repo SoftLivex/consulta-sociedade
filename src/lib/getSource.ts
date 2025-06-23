497ccb868b5ab01bddc6eb6f317483fad7f53c29
andrebrumdev <andre.brum@softlive>
2025-06-23 16:35:56 -0400

    refactor: simplify getSource function by removing environment-based prefix

--- src/lib/getSource.ts
+++ src/lib/getSource.ts
@@ -1,6 +1,6 @@
 export const getSource = (url: string) => {
     // is dev
-    const prefix = process.env.NODE_ENV === 'development' ? '' : '.';
+    const prefix = '';
 
     return `${prefix}${url}`;
 };
